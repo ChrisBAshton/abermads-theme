294a8f24e0e56ce56b4fb3b8afd86b83128921ed
ChrisBAshton <chrisashtonweb@gmail.com>
2016-10-10 14:15:54 +0100

    added 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); the_post(); ?>
+
+<div class="error error--404">
+    <h2>404</h2>
+    <p class="lead text-warning">
+        Sorry, the content you requested does not seem to exist.
+    </p>
+</div>
+
+<?php get_footer(); ?>
